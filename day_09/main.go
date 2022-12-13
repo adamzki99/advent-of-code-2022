4d50977ad6f4d695b69450c712406efdf334f9ea
Adam Svensson <adsv18@student.bth.se>
2022-12-13 22:12:20 +0100

    Part 2, complete

--- day_09/main.go
+++ day_09/main.go
@@ -9,67 +9,70 @@ import (
 	"strings"
 )
 
-func PerformHeadMove(ropeTailPositionsVisited *map[string]bool, headCoordinate *[]int, tailCoordinate *[]int, move []int) {
+func moveKnot(leadingKnot []int, knot *[]int) {
 
-	headCoordinateShadow := []int{0, 0}
+	if CalculateDistanceBetweenCoordinates(leadingKnot, *knot) < 2 {
+		return
+	}
 
-	if move[0] == 76 { // move left
-		for i := 0; i < move[1]; i++ {
-
-			copy(headCoordinateShadow, *headCoordinate)
-			(*headCoordinate)[0]--
-
-			if CalculateDistanceBetweenCoordinates(*headCoordinate, *tailCoordinate) > 1 {
-
-				copy(*tailCoordinate, headCoordinateShadow)
-				coordinate := fmt.Sprintln((*tailCoordinate)[0], (*tailCoordinate)[1])
-				(*ropeTailPositionsVisited)[coordinate] = true
-			}
+	if leadingKnot[0] == (*knot)[0] {
+		if leadingKnot[1] > (*knot)[1] {
+			(*knot)[1]++
+		} else {
+			(*knot)[1]--
 		}
-	} else if move[0] == 82 { // move right
-		for i := 0; i < move[1]; i++ {
-
-			copy(headCoordinateShadow, *headCoordinate)
-			(*headCoordinate)[0]++
-
-			if CalculateDistanceBetweenCoordinates(*headCoordinate, *tailCoordinate) > 1 {
-
-				copy(*tailCoordinate, headCoordinateShadow)
-				coordinate := fmt.Sprintln((*tailCoordinate)[0], (*tailCoordinate)[1])
-				(*ropeTailPositionsVisited)[coordinate] = true
-			}
+	} else if leadingKnot[1] == (*knot)[1] {
+		if leadingKnot[0] > (*knot)[0] {
+			(*knot)[0]++
+		} else {
+			(*knot)[0]--
 		}
-	} else if move[0] == 85 { // move up
-		for i := 0; i < move[1]; i++ {
+	} else {
 
-			copy(headCoordinateShadow, *headCoordinate)
-			(*headCoordinate)[1]++
-
-			if CalculateDistanceBetweenCoordinates(*headCoordinate, *tailCoordinate) > 1 {
-
-				copy(*tailCoordinate, headCoordinateShadow)
-				coordinate := fmt.Sprintln((*tailCoordinate)[0], (*tailCoordinate)[1])
-				(*ropeTailPositionsVisited)[coordinate] = true
-			}
+		if leadingKnot[0] > (*knot)[0] {
+			(*knot)[0]++
+		} else {
+			(*knot)[0]--
 		}
-	} else if move[0] == 68 { // move down
-		for i := 0; i < move[1]; i++ {
 
-			copy(headCoordinateShadow, *headCoordinate)
-			(*headCoordinate)[1]--
-
-			if CalculateDistanceBetweenCoordinates(*headCoordinate, *tailCoordinate) > 1 {
-
-				copy(*tailCoordinate, headCoordinateShadow)
-				coordinate := fmt.Sprintln((*tailCoordinate)[0], (*tailCoordinate)[1])
-				(*ropeTailPositionsVisited)[coordinate] = true
-			}
+		if leadingKnot[1] > (*knot)[1] {
+			(*knot)[1]++
+		} else {
+			(*knot)[1]--
 		}
 	}
 
 }
 
-func CalculateDistanceBetweenCoordinates(headCoordinate []int, tailCoordinate []int) int {
+func PerformHeadMove(headCoordinate *[]int, direction int) []int {
+
+	headCoordinateShadow := []int{0, 0}
+
+	if direction == 76 { // move left
+
+		copy(headCoordinateShadow, *headCoordinate)
+		(*headCoordinate)[0]--
+
+	} else if direction == 82 { // move right
+
+		copy(headCoordinateShadow, *headCoordinate)
+		(*headCoordinate)[0]++
+
+	} else if direction == 85 { // move up
+
+		copy(headCoordinateShadow, *headCoordinate)
+		(*headCoordinate)[1]++
+
+	} else if direction == 68 { // move down
+
+		copy(headCoordinateShadow, *headCoordinate)
+		(*headCoordinate)[1]--
+	}
+
+	return headCoordinateShadow
+}
+
+func CalculateDistanceBetweenCoordinates(headCoordinate []int, tailCoordinate []int) float64 {
 
 	diffX := headCoordinate[0] - tailCoordinate[0]
 	diffY := headCoordinate[1] - tailCoordinate[1]
@@ -77,8 +80,7 @@ func CalculateDistanceBetweenCoordinates(headCoordinate []int, tailCoordinate []
 	diffXSquared := math.Pow(float64(diffX), 2)
 	diffYSquared := math.Pow(float64(diffY), 2)
 
-	return int(math.Sqrt(diffXSquared + diffYSquared))
-
+	return math.Sqrt(diffXSquared + diffYSquared)
 }
 
 func main() {
@@ -112,14 +114,37 @@ func main() {
 		seriesOfMoves = append(seriesOfMoves, []int{moveDirection, moveSteps})
 	}
 
-	headCoordinate := []int{0, 0}
-	tailCoordinate := []int{0, 0}
 	ropeTailPositionsVisited := make(map[string]bool)
 
+	ropeKnots := [][]int{
+		{0, 0},
+		{0, 0},
+		{0, 0},
+		{0, 0},
+		{0, 0},
+		{0, 0},
+		{0, 0},
+		{0, 0},
+		{0, 0},
+		{0, 0},
+	}
+
 	for _, move := range seriesOfMoves {
-		PerformHeadMove(&ropeTailPositionsVisited, &headCoordinate, &tailCoordinate, move)
+		fmt.Println("==", string(move[0]), move[1], "==")
+		for i := 0; i < move[1]; i++ {
+
+			PerformHeadMove(&ropeKnots[0], move[0])
+
+			for k := 1; k < 10; k++ {
+
+				moveKnot(ropeKnots[k-1], &ropeKnots[k])
+			}
+
+			coordinate := fmt.Sprint(ropeKnots[9][0], ropeKnots[9][1])
+			(ropeTailPositionsVisited)[coordinate] = true
+
+		}
 	}
 
 	fmt.Println("Answer:", len(ropeTailPositionsVisited))
-
 }
