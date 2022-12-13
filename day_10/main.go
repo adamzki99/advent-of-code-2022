36008370aa55b0afe1598ec7f89b0baa1f5f25d1
Adam Svensson <adsv18@student.bth.se>
2022-12-14 00:00:02 +0100

    Part 2, complete, but bad

--- day_10/main.go
+++ day_10/main.go
@@ -8,15 +8,6 @@ import (
 	"strings"
 )
 
-func Contains(s []int, e int) bool {
-	for _, a := range s {
-		if a == e {
-			return true
-		}
-	}
-	return false
-}
-
 func BuildInteger(number string) int {
 
 	returnInteger := 0
@@ -30,6 +21,24 @@ func BuildInteger(number string) int {
 	}
 
 	return returnInteger
+}
+
+func DrawScreen(screenCRT [6][40]int) {
+
+	for _, row := range screenCRT {
+
+		line := ""
+		for _, pixel := range row {
+
+			if pixel == 1 {
+				line += "#"
+			} else {
+				line += "."
+			}
+		}
+		fmt.Println(line)
+	}
+
 }
 
 func main() {
@@ -49,20 +58,33 @@ func main() {
 	registerX := 1
 	cycleTracker := 0
 
-	specifiedCycles := []int{20, 60, 100, 140, 180, 220}
+	var screenCRT [6][40]int
 
-	signalStrengthSum := 0
+	drawingRow := 0
 
 	for fileScanner.Scan() {
 
 		line := strings.Split(fileScanner.Text(), " ")
 
 		for i := 0; i < 2; i++ {
-			cycleTracker++
 
-			if Contains(specifiedCycles, cycleTracker) {
-				signalStrengthSum += registerX * cycleTracker
+			if registerX-1 <= cycleTracker && cycleTracker <= registerX+1 {
+				screenCRT[drawingRow][cycleTracker] = 1
+			} else {
+				screenCRT[drawingRow][cycleTracker] = 0
 			}
+
+			if cycleTracker == 39 && cycleTracker > 0 {
+				drawingRow++
+				cycleTracker = -1
+
+				if drawingRow == 6 {
+					DrawScreen(screenCRT)
+					return
+				}
+			}
+
+			cycleTracker++
 
 			if line[0] == "noop" {
 				break
@@ -78,7 +100,5 @@ func main() {
 		}
 
 	}
-
-	fmt.Println("Answer:", signalStrengthSum)
 
 }
